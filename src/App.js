51dcf3b15e4ef36c950fcf024aff44270eb08cac
“viv-nic” <vivian1005@hotmail.com>
2022-11-18 21:20:51 +1100

    lesson 2 commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import List from './components/List/List';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React World!
-        </p>
+        <h1>
+          Vivian's Food Wish List
+          <List />
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
