7980f7c9c970089ecfa4b5ae695703019b7568af
“viv-nic” <vivian1005@hotmail.com>
2022-11-19 20:38:37 +1100

    remove original react app logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+
 import './styles.css';
 import List from './components/List/List';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Vivian's Food Wish List
-          <List />
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Vivian's Food Wish List</h1>
       </header>
+      <body>
+        <List />
+      </body>
+       
     </div>
   );
 }
