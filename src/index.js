ae863a2268f17a28141568fa38299909208c120c
“viv-nic” <vivian1005@hotmail.com>
2022-11-22 15:05:41 +1100

    lesson 4 routes and pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>
+  </BrowserRouter>
 );
