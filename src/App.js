4091dc6ee0b6c818ad92e24948965d3e7a41b734
“viv-nic” <vivian1005@hotmail.com>
2022-11-22 17:16:20 +1100

    lesson 4 routes and pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+import "./";
+// import Todo from "./components/todo";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path="/home">
+        Home
+      </Route>
+      <Route path="/about">
+        About
+      </Route>
     </div>
   );
 }
