53ed7efd92c905364d492aac8a801ef3c538ee08
“viv-nic” <vivian1005@hotmail.com>
2022-11-30 19:36:26 +1100

    add List to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./styles.css";
 import Todo from "./components/todo";
+import List from "./components/List/List";
 
 export default function App() {
   return (
     <main>
       <Todo />
+      <List />
     </main>
   );
 }
